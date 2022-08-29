b0305575b7d8ea973a88b25f9520b4d7c11bdc56
Harleen Dhatt <111666380+h-dhatt@users.noreply.github.com>
2022-08-29 06:09:58 -0400

    Update math.py

--- math.py
+++ math.py
@@ -26,10 +26,10 @@ def game():
     while True:
         if ask_question() == True:
             score += 1
-            print('Correct !')
+            print('Correct!')
         else:
-            print('Incorrect')
+            print('Incorrect.')
             break
-    print(f'======== Game Over ========\nYou score is {score}\nKepp going!')
+    print(f'======== Game Over ========\nYou score is {score}\nKeep going!')
 
 game()
